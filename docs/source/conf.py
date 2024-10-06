6e82ae1bcbfa522456c8bf2701b500a6da8b568f
Taher Chegini <cheginit@gmail.com>
2024-10-06 14:12:17 -0400

    MNT: Add rtd required code blocks.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,7 +35,14 @@ packages = {
     "pynldas2": "PyNLDAS2",
     "hydrosignatures": "HydroSignatures",
 }
+# Define the canonical URL if you are using a custom domain on Read the Docs
+html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "")
 
+# Tell Jinja2 templates the build is running on Read the Docs
+if os.environ.get("READTHEDOCS", "") == "True":
+    if "html_context" not in globals():
+        html_context = {}
+    html_context["READTHEDOCS"] = True
 # -- General configuration ---------------------------------------------------
 
 extensions = [
